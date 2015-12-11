1b62f33ef15c61831a01fd9b725db3510e9983dd
1ce7ea <cmstolper+github@gmail.com>
2015-12-11 12:54:16 -0700

    Update euler3.js

--- euler3.js
+++ euler3.js
@@ -1,36 +1,38 @@
 var primes = [2],
-    isPrime = true,
+	isPrime = true,
     j = 0,
     i = 3,
 
 largestPrimeFactor = function (someNumber) {
 
-  if (someNumber % 2 === 0) {
-    //console.log(someNumber + ' ' + primes[i]);
-    someNumber /= 2;
-  } else if (someNumber === 2) {
-    return someNumber;
+  while (someNumber % 2 === 0) {
+	//console.log(someNumber + ' ' + primes[j]);
+	someNumber /= 2;
   }
 
-  for (i; i <= someNumber/2; i += 2) {
-    
-    //console.log('check ' + i + ' divided by ' + primes[j]);
-    if (i % primes[j] === 0) {
-      isPrime = false;
-    } else {
-        //console.log('push');
-        primes.push(i);
-        j++;
-        while (someNumber % primes[i] === 0) {
-          //console.log(someNumber + ' ' + primes[i]);
-          someNumber /= primes[i];
-          if (someNumber === primes[i]) {
-            return someNumber;
-          }
-        }
-      }
-    
-  }
+  for (i; i*2 <= someNumber; i += 2) {
+
+        for (j = 0; primes[j] < i/2; j++) {
+			//console.log('check ' + i + ' divided by ' + primes[j] + ' - primes length is ' + primes.length);
+			if (i % primes[j] === 0) {
+				isPrime = false;
+			} else {
+				isPrime = true;
+			}
+		}
+		
+		if (isPrime) {
+			while (someNumber % primes[j] === 0) {
+				//console.log(someNumber + ' ' + primes[j]);
+				someNumber /= primes[j];
+				if (someNumber === primes[j]) {
+					return someNumber;
+				}
+			}
+			//console.log('push');
+			primes.push(i);
+		}
+	}
 
   return someNumber;
 };
