2f4bb06637ef855107767119571a200667670062
1ce7ea <cmstolper+github@gmail.com>
2015-12-18 12:30:16 -0700

    optimized

--- euler7.js
+++ euler7.js
@@ -1,35 +1,28 @@
-var primes = [2],
+function largestPrimeFactor (someNumber) {
+	
+	var primes = [2],
 	isPrime,
 	k = 1,
     j,
-    i = 3,
-
-largestPrimeFactor = function (someNumber) {
+    i = 3;
 
 	for (i; k < someNumber; i+=2) {
-		//console.log("Start: Is " + i + " prime? Test number is " + someNumber);
 		j = 0;
 		isPrime = true;
 		while (primes[j] < i/2) {
-			//console.log('check ' + i + ' divided by ' + primes[j]);
 			if (i % primes[j] === 0) {
 				isPrime = false;
 				j = i;
-				//console.log(i + " is NOT prime" );
 			} else {
 				j++;
-				//console.log(i + " is prime" );
 			}
-			//console.log("prime?: " + isPrime);
 		}
-		
 		if (isPrime) {
 			primes.push(i);
 			k++;
 		}
-		
 	}
 	return primes[k-1];
 };
 
-largestPrimeFactor(100);
+largestPrimeFactor(10001);
